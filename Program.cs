4728220e0dc3093b34ad1132dcc6e776dd4e82ea
nina-rebello <rebellonina@gmail.com>
2024-09-11 22:36:53 -0300

    implementando construtores e métodos faltantes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "78910", modelo: "Modelo 2", imei: "111111112", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
